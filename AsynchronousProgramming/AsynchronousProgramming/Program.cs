36ed7f236fd26fb21ba9a128fc4375b7242d2aea
Fahad <fahadmo0hammad@gmail.com>
2020-06-14 17:55:40 +0600

    add the program file with pouring Coffee.

---
+++ AsynchronousProgramming/AsynchronousProgramming/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coffee coffee = PourCoffe();
+            Console.WriteLine("Coffee Is Ready");
+
+            var egg = FryEggAsync(2);
+            var bacon = FryBaconAsync(2);
+        }
+
+        private static Bacon FryBaconAsync(int slices)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<Egg> FryEggAsync(int numnberOfEgg)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            await Task.Delay(5000);
+            Console.WriteLine($"Cracking {numnberOfEgg} eggs");
+            Console.WriteLine("Started cooking eggs");
+            await Task.Delay(5000);
+            Console.WriteLine("Eggs are ready..");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffe()
+        {
+            Console.WriteLine("Coffee Is Pouring");
+
+            return new Coffee();
+        }
+    }
+
+    internal class Bacon
+    {
+    }
+
+    internal class Egg
+    {
+    }
+
+    internal class Coffee
+    {
+    }
+}
